742d555b7ff6b2f19e318ad5b8a79626d6e469a2
santeripuranen <26481940+santeripuranen@users.noreply.github.com>
2018-08-20 21:27:01 +0300

    Make apegrunt_options.cmake more generic

--- apegrunt_options.cmake
+++ apegrunt_options.cmake
@@ -2,8 +2,8 @@
 
 cmake_minimum_required(VERSION 3.1)
 
-option( APEGRUNT_ENABLE_BOOST "Find Boost and, if successful, enable use in APEGRUNT" true )
-option( APEGRUNT_ENABLE_TBB "Find TBB and, if successful, enable use in APEGRUNT" true )
-option( APEGRUNT_ENABLE_COMPILER_INTRINSICS "Find compiler intrinsics headers and, if successful, enable use in APEGRUNT" true )
-option( APEGRUNT_ENABLE_GPROF "Generate instrumented binaries for profiling with gprof" false ) # off by default
-option( APEGRUNT_ENABLE_DOXYGEN "Find Doxygen and enable documentation generation" true ) 
+option( ${PROJECT_NAME}_ENABLE_BOOST "Find Boost and, if successful, enable use in ${PROJECT_NAME}" true )
+option( ${PROJECT_NAME}_ENABLE_TBB "Find TBB and, if successful, enable use in ${PROJECT_NAME}" true )
+option( ${PROJECT_NAME}_ENABLE_COMPILER_INTRINSICS "Find compiler intrinsics headers and, if successful, enable use in ${PROJECT_NAME}" true )
+option( ${PROJECT_NAME}_ENABLE_GPROF "Generate instrumented binaries for profiling with gprof" false ) # off by default
+option( ${PROJECT_NAME}_ENABLE_DOXYGEN "Find Doxygen and enable documentation generation" true ) 
