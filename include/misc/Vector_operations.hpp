0e179285b08fb345756894ab6bf777f142455c82
santeripuranen <26481940+santeripuranen@users.noreply.github.com>
2019-01-10 11:25:41 +0200

    Add new vector funtions and binary operators

--- include/misc/Vector_operations.hpp
+++ include/misc/Vector_operations.hpp
@@ -21,10 +21,141 @@
 #ifndef APEGRUNT_VECTOR_OPERATIONS_HPP
 #define APEGRUNT_VECTOR_OPERATIONS_HPP
 
+#include <type_traits> // for std::enable_if
+
 #include "Vector_forward.h"
 #include "Vector_interface.hpp"
 
 namespace apegrunt {
+
+template< typename RealT, std::size_t Capacity, bool View >
+inline RealT sum( const Vector<RealT,Capacity,View>& v )
+{
+	RealT s = 0;
+	for( std::size_t i=0; i < Capacity; ++i ) { s += v[i]; }
+	return s;
+}
+
+template< typename RealT, std::size_t Capacity, bool View, typename IntegerT >
+inline RealT mask_sum( const Vector<RealT,Capacity,View>& v, IntegerT mask )
+{
+	RealT s = 0;
+	const std::bitset<std::numeric_limits<IntegerT>::digits> m(mask);
+	for( std::size_t i=0; i < Capacity; ++i ) { s += m[i] ? v[i] : RealT(0); }
+	return s;
+}
+
+
+template< typename RealT, std::size_t Capacity, bool View >
+inline Vector<RealT,Capacity> log( const Vector<RealT,Capacity,View>& x )
+{
+	using std::log;
+	Vector<RealT,Capacity> v;
+	for( std::size_t i=0; i < Capacity; ++i ) { v[i] = log( x[i] ); }
+	return v;
+}
+
+template< typename RealT, std::size_t Capacity, bool View >
+inline Vector<RealT,Capacity> xaddlogx( const Vector<RealT,Capacity,View>& x )
+{
+	return x + log(x);
+}
+
+template< typename RealT, std::size_t Capacity, bool View >
+inline Vector<RealT,Capacity> xlogx( const Vector<RealT,Capacity,View>& x )
+{
+	return x * log(x);
+}
+
+template< typename RealT, std::size_t Capacity, bool View, typename IntegerT >
+inline Vector<RealT,Capacity> mask_xaddlogx( const Vector<RealT,Capacity,View>& x, IntegerT mask )
+{
+	using std::log;
+	Vector<RealT,Capacity> v;
+	const std::bitset<std::numeric_limits<IntegerT>::digits> m(mask);
+	for( std::size_t i=0; i < Capacity; ++i ) { v[i] = m[i] ? x[i] + log( x[i] ) : RealT(0); }
+	return v;
+}
+
+template< typename RealT, std::size_t Capacity, bool View, typename IntegerT >
+inline Vector<RealT,Capacity> mask_xlogx( const Vector<RealT,Capacity,View>& x, IntegerT mask )
+{
+	using std::log;
+	Vector<RealT,Capacity> v;
+	const std::bitset<std::numeric_limits<IntegerT>::digits> m(mask);
+	for( std::size_t i=0; i < Capacity; ++i ) { v[i] = m[i] ? x[i] * log( x[i] ) : RealT(0); }
+	return v;
+}
+
+// basic binary arithmetic operators
+template< typename RealT, std::size_t Capacity, bool aView, bool bView >
+inline Vector<RealT,Capacity> operator+( const Vector<RealT,Capacity,aView>& lhs, const Vector<RealT,Capacity,bView>& rhs )
+{
+	Vector<RealT,Capacity> v;
+	for( std::size_t i=0; i < Capacity; ++i ) { v[i] = lhs[i] + rhs[i]; }
+	return v;
+}
+
+template< typename RealT, std::size_t Capacity, bool aView, bool bView >
+inline Vector<RealT,Capacity> operator-( const Vector<RealT,Capacity,aView>& lhs, const Vector<RealT,Capacity,bView>& rhs )
+{
+	Vector<RealT,Capacity> v;
+	for( std::size_t i=0; i < Capacity; ++i ) { v[i] = lhs[i] - rhs[i]; }
+	return v;
+}
+
+template< typename RealT, std::size_t Capacity, bool aView, bool bView >
+inline Vector<RealT,Capacity> operator*( const Vector<RealT,Capacity,aView>& lhs, const Vector<RealT,Capacity,bView>& rhs )
+{
+	Vector<RealT,Capacity> v;
+	for( std::size_t i=0; i < Capacity; ++i ) { v[i] = lhs[i] * rhs[i]; }
+	return v;
+}
+
+template< typename RealT, std::size_t Capacity, bool aView, bool bView >
+inline Vector<RealT,Capacity> operator/( const Vector<RealT,Capacity,aView>& lhs, const Vector<RealT,Capacity,bView>& rhs )
+{
+	Vector<RealT,Capacity> v;
+	for( std::size_t i=0; i < Capacity; ++i ) { v[i] = lhs[i] / rhs[i]; }
+	return v;
+}
+
+// bitshift integer fields in lhs by the amount of steps specified by fields in rhs
+template< typename RealT, std::size_t Capacity, bool View, typename StateT > // should use std::enable_if to drop from overload resolution if not std::is_integral<RealT>::value
+inline Vector<RealT,Capacity> operator<< ( Vector<RealT,Capacity,View> lhs, State_block<StateT,Capacity> rhs )
+{
+	Vector<RealT,Capacity> v;
+	for( std::size_t i = 0; i < Capacity; ++i )
+	{
+		v[i] = lhs[i] << std::size_t( rhs[i] );
+	}
+	return v;
+}
+
+// bitwise AND for array elements
+template< typename RealT, std::size_t Capacity, bool View, bool View2 >
+inline Vector<RealT,Capacity> operator| ( Vector<RealT,Capacity,View> lhs, Vector<RealT,Capacity,View2> rhs )
+{
+	Vector<RealT,Capacity> v;
+	for( std::size_t i = 0; i < Capacity; ++i )
+	{
+		v[i] = lhs[i] | rhs[i];
+	}
+	return v;
+}
+
+template< typename RealT, std::size_t Capacity, bool View >
+inline Vector<RealT,Capacity> popcnt_per_element( Vector<RealT,Capacity,View> a )
+{
+	Vector<RealT,Capacity> v;
+	for( std::size_t i = 0; i < Capacity; ++i )
+	{
+		// OK, this is a lazy solution, but it works
+		v[i] = std::bitset<std::numeric_limits<RealT>::digits>(a[i]).count();
+	}
+	return v;
+}
+
 
 #ifndef NO_INTRINSICS
 #ifdef __AVX__
@@ -40,22 +171,17 @@ inline double sum( __m256d a )
 	return typename vec_t::element_t( d.m_elem[0] );
 }
 
+inline double sum( const Vector<double,4,false>& v )
+{
+	return sum( v() ); // forward to sum( __m256d a )
+}
+
 template< uint Exponent >
-inline __m256d pow( __m256d x ) { __m256d result = x; for( std::size_t i=1; i<Exponent; ++i ) { result = result * x; } return result; }
+inline constexpr __m256d pow( __m256d x ) { __m256d result = x; for( std::size_t i=1; i<Exponent; ++i ) { result = result * x; } return result; }
 template<>
-inline __m256d pow<2>( __m256d x ) { return x*x; }
+inline constexpr __m256d pow<2>( __m256d x ) { return x*x; }
 
 #endif // __AVX__
-
-template< typename RealT, uint Capacity, bool View >
-inline RealT sum( const Vector<RealT,Capacity,View>& v )
-{
-	RealT s = 0;
-	std::cout << "[";
-	for( std::size_t i=0; i < Capacity; ++i ) { s += v[i]; std::cout << " " << v[i];}
-	std::cout << " ] sum=" << s << "\n";
-	return s;
-}
 
 #endif // #ifndef NO_INTRINSICS
 
