849f423bf86a1596e83b32a8bfac21402eee1066
santeripuranen <26481940+santeripuranen@users.noreply.github.com>
2019-01-15 17:15:46 +0200

    Fix EdgeID behaviour

--- include/graph/Graph.hpp
+++ include/graph/Graph.hpp
@@ -26,53 +26,57 @@
 #include <set>
 #include <cmath>
 #include <mutex> // for std::mutex
+#include <limits>
 
 #include "Graph_forward.h"
 #include "apegrunt/Apegrunt_utility.hpp"
 
 namespace apegrunt {
 
+template< typename IntegerT >
+constexpr IntegerT create_flag_mask() { return IntegerT(1) << (std::numeric_limits<IntegerT>::digits-1); }
+
 struct EdgeID : public apegrunt::extend_comparison_operators
 {
-	using node_id_t = int32_t;
-	using id_t = int64_t;
+	using node_id_t = uint32_t;
+	using id_t = uint64_t;
 	using my_type = EdgeID;
 
-	enum { FLAG_MASK=-id_t(0) };
-
-	//EdgeID() : m_id(0) { }
+	enum { ID_FLAG_MASK = create_flag_mask<id_t>() };
+	enum { ID_UNFLAG_MASK = ~create_flag_mask<id_t>() };
+	enum { NODE_ID_UNFLAG_MASK = ~create_flag_mask<node_id_t>() };
 
 	EdgeID( node_id_t i, node_id_t j ) : m_i( std::min(i,j) ), m_j( std::max(i,j) )
 	{
 		//std::cout << "EdgeID(): m_i=" << m_i << " m_j=" << m_j << " m_id=" << m_id << std::endl;
 	}
-	EdgeID( id_t id ) : m_id(id) { }
+	constexpr EdgeID( id_t id ) : m_id(id) { }
 	~EdgeID() = default;
 
-	EdgeID( const my_type& other ) : m_id( other.m_id ) { }
-	EdgeID( my_type&& other ) : m_id( std::move(other.m_id) ) { }
+	constexpr EdgeID( const my_type& other ) : m_id( other.m_id ) { }
+	constexpr EdgeID( my_type&& other ) : m_id( std::move(other.m_id) ) { }
 
 	constexpr my_type& operator=( const my_type& other ) { m_id = other.m_id; }
 
 	union
 	{
 		struct {
-			node_id_t m_i; // we use sign as flag bit
-			node_id_t m_j;
+			node_id_t m_i;
+			node_id_t m_j; // we use sign as flag bit
 		};
 		id_t m_id;
 	};
 
-	inline node_id_t first() const { return std::abs(m_i); }
-	inline node_id_t second() const { return std::abs(m_j); }
-	inline id_t id() const { return std::abs(m_id); }
+	inline constexpr node_id_t first() const { return m_i & NODE_ID_UNFLAG_MASK; }
+	inline constexpr node_id_t second() const { return m_j & NODE_ID_UNFLAG_MASK; }
+	inline constexpr const id_t& id() const { return m_id; } // return "m_id & ID_UNFLAG_MASK" if flag not desired
 
-	inline constexpr operator bool() const { return std::signbit(m_id); }
-	inline constexpr void set() { m_id = m_id | FLAG_MASK; }
-	inline constexpr void unset() { m_id = m_id ^ FLAG_MASK; }
+	inline constexpr operator bool() const { return m_id & ID_FLAG_MASK; }
+	inline constexpr void set() { m_id = m_id | ID_FLAG_MASK; }
+	inline constexpr void unset() { m_id = m_id & ID_UNFLAG_MASK; }
 
-	inline bool operator==( const my_type& rhs ) const { return this->id() == rhs.id(); }
-	inline bool operator<( const my_type& rhs ) const { return this->id() < rhs.id(); }
+	inline constexpr bool operator==( const my_type& rhs ) const { return this->id() == rhs.id(); }
+	inline constexpr bool operator<( const my_type& rhs ) const { return this->id() < rhs.id(); }
 };
 
 static std::ostream& operator<< ( std::ostream& os, const EdgeID& id )
@@ -101,16 +105,16 @@ struct Edge : public apegrunt::extend_comparison_operators
 	id_t m_id;
 	real_t m_w;
 
-	inline id_t id() const { return m_id; }
-	inline typename id_t::node_id_t node1() const { return m_id.first(); }
-	inline typename id_t::node_id_t node2() const { return m_id.second(); }
+	inline constexpr id_t id() const { return m_id; }
+	inline constexpr typename id_t::node_id_t node1() const { return m_id.first(); }
+	inline constexpr typename id_t::node_id_t node2() const { return m_id.second(); }
 	inline constexpr real_t weight() const { return m_w; }
 	inline constexpr operator bool() const { return bool(m_id); }
 	inline constexpr void set() { m_id.set(); }
 	inline constexpr void unset() { m_id.unset(); }
 
-	inline bool operator==( const my_type& rhs ) const { return m_id == rhs.m_id; }
-	inline bool operator<( const my_type& rhs ) const { return m_id < rhs.m_id; }
+	inline constexpr bool operator==( const my_type& rhs ) const { return m_id == rhs.m_id; }
+	inline constexpr bool operator<( const my_type& rhs ) const { return m_id < rhs.m_id; }
 };
 
 static std::ostream& operator<< ( std::ostream& os, const Edge& edge )
@@ -142,38 +146,40 @@ public:
 	~Graph() = default;
 
 	Graph( my_type&& other )
-	: m_nodes( std::move(other.m_nodes) ),
+	: //m_nodes( std::move(other.m_nodes) ),
 	  m_edges( std::move(other.m_edges) )
 	{
 	}
 
+	// Add a now node. Caller should lock before calling add(), if used in a parallel setting.
 	inline void add( node_id_t i, node_id_t j, real_t weight )
 	{
-		//std::cout << "apegrunt::Graph.add(" << i << "," << j << "," << weight << ")" << std::endl;
-		//m_modify.lock();
-		//std::cout << "apegrunt::Graph: insert node" << std::endl;
-		//m_nodes.insert(i); m_nodes.insert(j);
-		//std::cout << "apegrunt::Graph: insert edge" << std::endl;
 		m_edges.emplace_back( i, j, weight );
-		//m_modify.unlock();
 	}
 
-	void join( my_type& other )
+	inline void join( my_type& other )
 	{
-		auto&& scope_lock = this->lock();
-		other.lock();
+		//auto&& scope_lock_this = this->acquire_lock();
+		//auto&& scope_lock_other = other.acquire_lock();
+		this->lock(); other.lock();
+		//std::cout << "apegrunt: Graph.join() - this->size()=" << this->size() << " other.size()=" << other.size() << std::endl;
 		this->reserve( this->size()+other.size() );
 		for( auto& edge: other ) { m_edges.push_back( edge ); }
+		other.clear();
+		other.unlock(); this->unlock();
 	}
 
-	inline lock_t lock() { return std::move( lock_t(m_modify) ); }
+	inline lock_t acquire_lock() { lock_t lock(m_modify); lock.lock(); return std::move( lock ); }
+	inline void lock() { m_modify.lock(); }
+	inline void unlock() { m_modify.unlock(); }
+	inline void clear() { m_edges.clear(); }
 
 	inline node_id_t capacity() const { return m_edges.capacity(); }
 	inline void reserve( std::size_t n ) { m_edges.reserve(n); }
 	inline std::size_t size() const { return m_edges.size(); }
 
 	// sort in descending order of weight
-	inline void sort() { std::sort( this->begin(), this->end(), [](edge_t a, edge_t b) { return a.m_w > b.m_w; } ); }
+	inline void sort() { std::sort( this->begin(), this->end(), [](edge_t a, edge_t b) { return (a.weight() == b.weight()) ? (a.id() > b.id()) : (a.weight() > b.weight()); } ); }
 
 	inline edge_itr_t begin() { using std::begin; return begin(m_edges); }
 	inline edge_itr_t end() { using std::end; return end(m_edges); }
@@ -182,7 +188,7 @@ public:
 	inline edge_const_itr_t end() const { using std::cend; return cend(m_edges); }
 
 private:
-	node_storage_t m_nodes;
+	//node_storage_t m_nodes;
 	edge_storage_t m_edges;
 	std::mutex m_modify;
 };
