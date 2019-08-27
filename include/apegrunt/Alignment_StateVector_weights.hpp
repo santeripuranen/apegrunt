1d47c199fbfb1e91b036dc7c9777d424cea21998
santeripuranen <26481940+santeripuranen@users.noreply.github.com>
2019-08-27 12:43:15 +0300

    Added a faster sample weight routine w/ a smaller memory footprint.

--- include/apegrunt/Alignment_StateVector_weights.hpp
+++ include/apegrunt/Alignment_StateVector_weights.hpp
@@ -22,67 +22,14 @@
 #ifndef APEGRUNT_ALIGNMENT_STATEVECTOR_WEIGHTS_HPP
 #define APEGRUNT_ALIGNMENT_STATEVECTOR_WEIGHTS_HPP
 
+#include <memory> // for std::shared_ptr and std::make_shared
+
 #include "misc/Stopwatch.hpp"
 #include "StateVector_forward.h"
 
+#include "misc/VantagePointTree.hpp"
+
 namespace apegrunt {
-
-template< typename StateT, typename RealT=double >
-std::vector<RealT> calculate_weights( const apegrunt::Alignment_ptr<StateT> alignment )
-{
-	stopwatch::stopwatch cputimer(Apegrunt_options::get_out_stream()); // for timing statistics
-
-	using real_t = RealT;
-
-	const std::size_t n_seqs = alignment->size();
-	auto ali = alignment->subscript_proxy();
-
-	std::vector<std::size_t> int_weights; int_weights.reserve( n_seqs );
-	for( const auto& seq_ptr: *alignment ) { int_weights.push_back( seq_ptr->multiplicity() ); }
-
-	if( Apegrunt_options::verbose() ) { *Apegrunt_options::get_out_stream() << "apegrunt: calculate weights"; Apegrunt_options::get_out_stream()->flush(); }
-	cputimer.start();
-	std::size_t nonidentical_pairs = 0;
-
-	//std::size_t sum = 0;
-	for( std::size_t i = 0; i < n_seqs; ++i )
-	{
-		const auto seq_i = ali[i]; // (*alignment)[i];
-		const auto seq_i_size = seq_i->size();
-		const auto seq_i_multiplicity = seq_i->multiplicity();
-		for( std::size_t j = 0; j < i; ++j )
-		{
-			if( seq_i->is_similar_to( *(ali[j]), Apegrunt_options::sample_reweighting_threshold()*double(std::min(seq_i_size,ali[j]->size())) ) )
-			{
-				int_weights[i] += ali[j]->multiplicity();
-				int_weights[j] += seq_i_multiplicity;
-			}
-			else
-			{
-				++nonidentical_pairs;
-			}
-		}
-	}
-	cputimer.stop();
-	if( Apegrunt_options::verbose() ) { cputimer.print_timing_stats(); }
-
-	if( Apegrunt_options::verbose() )
-	{
-		const std::size_t threshold = Apegrunt_options::sample_reweighting_threshold() * double(alignment->n_loci());
-		const auto total_pairs = (n_seqs*(n_seqs-1))/2;
-		*Apegrunt_options::get_out_stream()
-			<< "apegrunt: reweighting threshold=" << threshold << "\n"
-			<< "apegrunt: # of non-identical pairs=" << nonidentical_pairs << " out of " << total_pairs << " (ratio=" << real_t(nonidentical_pairs)/real_t(total_pairs) << ")\n";
-	}
-
-	std::vector<real_t> weights; weights.reserve(n_seqs);
-
-	using boost::get;
-	for( auto&& int_weights_and_seq : zip_range(int_weights,*alignment) ) { weights.push_back( get<1>(int_weights_and_seq)->multiplicity() / real_t( get<0>(int_weights_and_seq) ) ); } // 1/weight * multiplicity
-	//for( auto&& int_weights_and_seq : zip_range(int_weights,*alignment) ) { weights.push_back( ( ( get<1>(int_weights_and_seq)->multiplicity() / real_t( get<0>(int_weights_and_seq) ) ) / real_t(sum) ) * real_t(n_seqs) ); } // 1/weight * multiplicity
-
-	return weights;
-}
 
 template< typename StateT, typename RealT=double >
 std::vector<RealT> calculate_weights_w_cached_distance_matrix( apegrunt::Alignment_ptr<StateT> alignment )
@@ -146,6 +93,133 @@ std::vector<RealT> calculate_weights_w_cached_distance_matrix( apegrunt::Alignme
 		const auto total_pairs = (n_seqs*(n_seqs-1))/2;
 		*Apegrunt_options::get_out_stream()
 			<< "apegrunt: sequence identity min=" << min_identity << " max=" << max_identity << " reweighting threshold=" << threshold << "\n"
+			<< "apegrunt: # of non-identical pairs=" << nonidentical_pairs << " out of " << total_pairs << " (ratio=" << real_t(nonidentical_pairs)/real_t(total_pairs) << ")\n";
+	}
+
+	std::vector<real_t> weights; weights.reserve(n_seqs);
+
+	using boost::get;
+	for( auto&& int_weights_and_seq : zip_range(int_weights,*alignment) ) { weights.push_back( get<1>(int_weights_and_seq)->multiplicity() / real_t( get<0>(int_weights_and_seq) ) ); } // 1/weight * multiplicity
+	//for( auto&& int_weights_and_seq : zip_range(int_weights,*alignment) ) { weights.push_back( ( ( get<1>(int_weights_and_seq)->multiplicity() / real_t( get<0>(int_weights_and_seq) ) ) / real_t(sum) ) * real_t(n_seqs) ); } // 1/weight * multiplicity
+
+	return weights;
+}
+
+template< typename StateT, typename RealT=double >
+std::vector<RealT> calculate_weights_wo_cached_distance_matrix( apegrunt::Alignment_ptr<StateT> alignment )
+{
+	stopwatch::stopwatch cputimer(Apegrunt_options::get_out_stream()); // for timing statistics
+
+	using real_t = RealT;
+
+	auto ali = alignment->subscript_proxy();
+
+	const real_t threshold = Apegrunt_options::sample_reweighting_threshold();
+	const std::size_t n_seqs = alignment->size();
+
+	std::vector<std::size_t> int_weights; int_weights.reserve( n_seqs );
+	for( const auto& seq_ptr: *alignment ) { int_weights.push_back( seq_ptr->multiplicity() ); }
+
+	real_t min_identity = 1.0;
+	real_t max_identity = 0.0;
+
+	if( Apegrunt_options::verbose() ) { *Apegrunt_options::get_out_stream() << "apegrunt: calculate weights (w/o cached distance matrix)"; Apegrunt_options::get_out_stream()->flush(); }
+	cputimer.start();
+	std::size_t nonidentical_pairs = 0;
+	//std::size_t sum = 0;
+	for( std::size_t i = 0; i < n_seqs; ++i )
+	{
+		const auto seq_i = ali[i]; // (*alignment)[i];
+		const auto seq_i_size = seq_i->size();
+		const auto seq_i_multiplicity = seq_i->multiplicity();
+		for( std::size_t j = 0; j < i; ++j )
+		{
+
+			const std::size_t n_elem = std::min( seq_i_size, ali[j]->size() );
+			const auto identity = real_t( *seq_i && *(ali[j]) ) / real_t(n_elem); // bitwise-AND counts the number of identical positions
+			min_identity = std::min( min_identity, identity );
+			max_identity = std::max( max_identity, identity );
+
+			if( identity < threshold )
+			{
+				++nonidentical_pairs;
+			}
+			else
+			{
+				//int_weights[i] += (*alignment)[j]->multiplicity();
+				int_weights[i] += ali[j]->multiplicity();
+				int_weights[j] += seq_i_multiplicity;
+				//sum += ( ali[j]->multiplicity() + seq_i_multiplicity );
+			}
+		}
+	}
+	cputimer.stop();
+	if( Apegrunt_options::verbose() ) { cputimer.print_timing_stats(); }
+
+	if( Apegrunt_options::verbose() )
+	{
+		const auto total_pairs = (n_seqs*(n_seqs-1))/2;
+		*Apegrunt_options::get_out_stream()
+			<< "apegrunt: sequence identity min=" << min_identity << " max=" << max_identity << " reweighting threshold=" << threshold << "\n"
+			<< "apegrunt: # of non-similar pairs=" << nonidentical_pairs << " out of " << total_pairs << " (ratio=" << real_t(nonidentical_pairs)/real_t(total_pairs) << ")\n";
+	}
+
+	std::vector<real_t> weights; weights.reserve(n_seqs);
+
+	using boost::get;
+	for( auto&& int_weights_and_seq : zip_range(int_weights,*alignment) ) { weights.push_back( get<1>(int_weights_and_seq)->multiplicity() / real_t( get<0>(int_weights_and_seq) ) ); } // 1/weight * multiplicity
+	//for( auto&& int_weights_and_seq : zip_range(int_weights,*alignment) ) { weights.push_back( ( ( get<1>(int_weights_and_seq)->multiplicity() / real_t( get<0>(int_weights_and_seq) ) ) / real_t(sum) ) * real_t(n_seqs) ); } // 1/weight * multiplicity
+
+	return weights;
+}
+
+template< typename StateT, typename RealT=double >
+std::vector<RealT> calculate_weights( const apegrunt::Alignment_ptr<StateT> alignment )
+{
+	stopwatch::stopwatch cputimer(Apegrunt_options::get_out_stream()); // for timing statistics
+
+	using real_t = RealT;
+
+	auto ali = alignment->subscript_proxy();
+
+	const std::size_t n_seqs = alignment->size();
+	const std::size_t L = alignment->n_loci();
+	const std::size_t threshold = Apegrunt_options::sample_reweighting_threshold() * double(L);
+
+	std::vector<std::size_t> int_weights; int_weights.reserve( n_seqs );
+	for( const auto& seq_ptr: *alignment ) { int_weights.push_back( seq_ptr->multiplicity() ); }
+
+	if( Apegrunt_options::verbose() ) { *Apegrunt_options::get_out_stream() << "apegrunt: calculate weights (w/o cached distance matrix)"; Apegrunt_options::get_out_stream()->flush(); }
+	cputimer.start();
+	std::size_t nonidentical_pairs = 0;
+
+	for( std::size_t i = 0; i < n_seqs; ++i )
+	{
+		const auto seq_i = ali[i]; // (*alignment)[i];
+		const auto seq_i_size = seq_i->size();
+		const auto seq_i_multiplicity = seq_i->multiplicity();
+		for( std::size_t j = 0; j < i; ++j )
+		{
+			if( seq_i->is_similar_to( *(ali[j]), Apegrunt_options::sample_reweighting_threshold()*double(std::min(seq_i_size,ali[j]->size())) ) )
+			{
+				//int_weights[i] += (*alignment)[j]->multiplicity();
+				int_weights[i] += ali[j]->multiplicity();
+				int_weights[j] += seq_i_multiplicity;
+				//sum += ( ali[j]->multiplicity() + seq_i_multiplicity );
+			}
+			else
+			{
+				++nonidentical_pairs;
+			}
+		}
+	}
+	cputimer.stop();
+	if( Apegrunt_options::verbose() ) { cputimer.print_timing_stats(); }
+
+	if( Apegrunt_options::verbose() )
+	{
+		const auto total_pairs = (n_seqs*(n_seqs-1))/2;
+		*Apegrunt_options::get_out_stream()
 			<< "apegrunt: # of non-identical pairs=" << nonidentical_pairs << " out of " << total_pairs << " (ratio=" << real_t(nonidentical_pairs)/real_t(total_pairs) << ")\n";
 	}
 
