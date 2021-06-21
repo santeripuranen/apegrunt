4eb0b8311af23a3c5f5d1d090ea1cfa0ef49a378
santeripuranen <26481940+santeripuranen@users.noreply.github.com>
2021-06-21 22:06:29 +0300

    Make it compile in c++17

--- include/apegrunt/Loci_parser_grammar.hpp
+++ include/apegrunt/Loci_parser_grammar.hpp
@@ -36,7 +36,6 @@
 
 #include "Parser_commons.hpp"
 #include "Loci_forward.h"
-#include "Loci_impl_default_storage.hpp"
 
 namespace apegrunt {
 
@@ -46,7 +45,7 @@ namespace qi	= boost::spirit::qi;
 namespace ascii	= boost::spirit::ascii;
 namespace phx	= boost::phoenix;
 
-template< typename IteratorT, typename LociT=apegrunt::Loci_impl_default_storage<std::size_t> >
+template< typename IteratorT, typename LociT >
 struct Loci_parser_grammar
 	: qi::grammar< IteratorT, Loci_ptr(), qi::locals< std::shared_ptr<LociT> >, ascii::space_type >
 {
@@ -63,11 +62,6 @@ struct Loci_parser_grammar
 	explicit Loci_parser_grammar( std::size_t base_index=1 )
 		: Loci_parser_grammar::base_type(start, "Loci_parser_grammar"), m_base_index(base_index)
 	{
-		using apegrunt::parsers::printer;
-
-		//loci_count %= qi::_int
-		//;
-
 		single_locus = ( qi::uint_ - '-' )[ phx::bind( static_cast<push_back>(&loci_t::push_back), qi::_r1, qi::_1, m_base_index ) ] // 1 is the second argument of the bound member function; the default value for that argument is apparently not enought, but an explicit value is needed to compile
 		;
 
@@ -78,38 +72,26 @@ struct Loci_parser_grammar
 		;
 
 		// Main node
-		start =	//qi::eps[ phx::bind( &index_base, qi::_r1 )
-				/* a simple "qi::_a = std::make_shared<T>()" would not be lazy,
+		start =	/* a simple "qi::_a = std::make_shared<T>()" would not be lazy,
 				instead it's evaluated immediately and only once. lazy_make_shared evaluates
 				lazily each time the parent parser is successfully called. */
 				qi::eps[ qi::_a = apegrunt::parsers::lazy_make_shared<loci_t>()() ]
-			 //>> loci_count[ qi::_b = phx::bind( &loci_t::reserve, phx::bind( &loci_ptr_t::get, qi::_a ), qi::_1 ) ] // pass a raw ptr
-			 >> *( loci( phx::bind( &loci_ptr_t::get, qi::_a ) ) ) // pass a raw ptr
+			 //>> *loci( phx::bind( &loci_ptr_t::get, qi::_a ) ) // pass a raw ptr
+			 >> *loci( qi::_a ) // pass the shared_ptr
 			 >> qi::eps[ qi::_val = qi::_a ]
 		;
-/*
-		qi::on_error<qi::fail>(start,
-		   std::cerr << phx::val("Expected ") << qi::_4 << phx::val(" at offset ")
-		      << (qi::_3 - qi::_1) << phx::val(" in ") << std::string(qi::_1, qi::_2)
-		      << std::endl)
-		;
-*/
 	}
 
-//	qi::rule<IteratorT, std::size_t(), space_t>
-//		loci_count
-//	;
-
-	qi::rule<IteratorT, void(loci_t* const), space_t>
+	//qi::rule<IteratorT, void(loci_t* const), space_t>
+	qi::rule<IteratorT, void(loci_ptr_t), space_t>
 		single_locus,
 		loci_range,
 		loci
 	;
 
-	qi::rule< IteratorT, Loci_ptr(), qi::locals< std::shared_ptr<loci_t> >, ascii::space_type > start;
+	qi::rule< IteratorT, Loci_ptr(), qi::locals< loci_ptr_t >, ascii::space_type > start;
 
 	std::size_t m_base_index;
-
 };
 
 } // namespace parsers
